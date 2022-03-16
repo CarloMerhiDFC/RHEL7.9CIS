da6675371c34dfdd8638b9f86151409935ca1198
CarloMerhiDFC <99970060+CarloMerhiDFC@users.noreply.github.com>
2022-03-16 09:09:21 -0400

    Update rhel_config_1.sh

--- rhel_config_1.sh
+++ rhel_config_1.sh
@@ -6,4 +6,4 @@ RED='\033[1;31m'
 echo
 echo -e "${BLUE}Installing BigFix Client"
 mkdir /etc/opt/BESClient/
-rpm -ivh 'https://software.bigfix.com/download/bes/100/BESAgent-10.0.5.50-rhe6.x86_64.rpm'
+rpm -ivh https://software.bigfix.com/download/bes/100/BESAgent-10.0.5.50-rhe6.x86_64.rpm
